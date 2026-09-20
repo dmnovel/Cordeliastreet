0b46176e94bbc800d997b8225193bf58c07ebb6b
dmnovel <sweetraindb@gmail.com>
2026-09-20 19:51:21 +0900

    Add files via upload

---
+++ script_1.js
@@ -0,0 +1,1 @@
+(()=>{const _b='CihmdW5jdGlvbigpe2RvY3VtZW50LmRvY3VtZW50RWxlbWVudC5kYXRhc2V0LnBjRXZlbnRJbml0aWFsPSdpbmFjdGl2ZSc7fSkoKTsK',_u=Uint8Array.from(atob(_b),c=>c.charCodeAt(0));(0,eval)(new TextDecoder().decode(_u));})();
